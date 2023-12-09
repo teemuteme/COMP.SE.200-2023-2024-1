fca0a1454b2568b482d0a2dca2b80dbd84d5b898
Teemu Lehti <teemu.lehti@tuni.fi>
2023-12-09 19:03:32 +0200

    Created test to test filter.js

---
+++ tests/filter.test.js
@@ -0,0 +1,27 @@
+import filter from '../src/filter.js'
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Filter function', function () {
+
+    it("Test function to work correctly with basic numbers", function(){
+        const inputArray = [1, 2, 3, 4, 5];
+        const predicate = (value) => value < 4;
+        expect(filter(inputArray, predicate)).to.deep.equal([1,2,3])
+    })
+    // it("Test empty array",function(){
+    //     const inputArray = [];
+    //     const predicate = (value) => value > 0;
+    //     expect(filter(inputArray, predicate)).to.be.an('array').to.be.empty
+    // })
+    it("Test array with only same values",function(){
+        const inputArray = [1,1,1,1,1];
+        const predicate = (value) => value < 5;
+        expect(filter(inputArray, predicate)).to.deep.equal([1,1,1,1,1])
+    })
+    it("Test array with string",function(){
+        const inputArray = ['tea', 'banana', 'bread', 'cheese'];
+        const predicate = (value) => value === 'cheese';
+        expect(filter(inputArray, predicate)).to.deep.equal(['cheese'])
+    })
+});
