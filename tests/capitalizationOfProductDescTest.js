c25c2873d6f044c876becaec43b7465f2459c593
Teemu Lehti <teemu.lehti@tuni.fi>
2023-12-10 18:25:00 +0200

    Created test capitalizationOfProductDesc

---
+++ tests/capitalizationOfProductDescTest.js
@@ -0,0 +1,45 @@
+import capitalize from '../src/capitalize.js'
+import toString from '../src/toString.js'
+import upperFirst from '../src/upperFirst.js'
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Capitalization of product descriptions', function () {
+
+    it("Test toString works when data is number", function(){
+        const value = 100;
+        const string = toString(value)
+        expect(toString(string)).to.be.a('string')
+    })
+    it("Test toString works when data is string", function(){
+        const value = "test";
+        const string = toString(value)
+        expect(toString(string)).to.be.a('string')
+    })
+
+    it("Test upperFirst if string is all lowercase", function(){
+        const test = "test"
+        expect(upperFirst(test)).to.be.deep.equal("Test")
+    })
+    it("Test upperFirst if string is all uppercase", function(){
+        const test = "TEST"
+        expect(upperFirst(test)).to.be.deep.equal("TEST")
+    })
+    it("Test upperFirst to work correctly when string has whitespaces", function(){
+        const test = "test description"
+        expect(upperFirst(test)).to.be.deep.equal("Test description")
+    })
+
+    it("Test capitalize to work correctly when all letters are lowercase", function(){
+        const test = "test"
+        expect(capitalize(test)).to.be.deep.equal("Test")
+    })
+    it("Test capitalize to work correctly when all letters are uppercase", function(){
+        const test = "TEST"
+        expect(capitalize(test)).to.be.deep.equal("Test")
+    })
+    it("Test capitalize to work correctly when string has whitespaces", function(){
+        const test = "test DESCRIPTION to TEST capitalize"
+        expect(capitalize(test)).to.be.deep.equal("Test description to test capitalize")
+    })
+})
